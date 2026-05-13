a9bbdefc7bca0565c26cd6eafdd87c38b9c71eb6
Tagatte DIOP <tagatte.diop@etudiant.univ-rennes.fr>
2026-05-13 23:30:52 +0200

    feat(frontend): validation email et téléphone dans les formulaires
    - Ajout validation email avec regex dans UserForm, ArtistForm, OrganizerForm
    - Ajout validation téléphone (10 chiffres) dans les formulaires
    - Correction des chemins d'import vers src/app/service/
    - Ajout messages d'erreur inline

--- src/app/pages/users/user-form/user-form.ts
+++ src/app/pages/users/user-form/user-form.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, inject, signal, ChangeDetectorRef} from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { User } from '../../../models/user';
 import { UserService } from '../../../services/user-service';
-
+import { User } from '../../../models/user';
 
 @Component({
   selector: 'app-user-form',
@@ -23,38 +22,55 @@ export class UserForm implements OnInit {
   lastname = '';
   email = '';
   phone = '';
-  
 
-  
   ngOnInit(): void {
-  const id = this.route.snapshot.paramMap.get('id');
-  if (id) {
-    this.isEditMode = true;
-    this.userId = +id;
-    this.userService.getUser(this.userId).subscribe((user: User) => {
-      this.firstname = user.firstname;
-      this.lastname = user.lastname;
-      this.email = user.email;
-      this.phone = user.phone;
-      this.cdr.detectChanges();
-    });
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.isEditMode = true;
+      this.userId = +id;
+      this.userService.getUser(this.userId).subscribe((user: User) => {
+        this.firstname = user.firstname;
+        this.lastname = user.lastname;
+        this.email = user.email;
+        this.phone = user.phone;
+        this.cdr.detectChanges();
+      });
+    }
   }
 
-}
+  isValidEmail(): boolean {
+    const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
+    return emailRegex.test(this.email);
+  }
 
- submit(): void {
-  if (this.isEditMode && this.userId) {
-    const user = new User(this.firstname, this.lastname, this.email, this.phone, this.userId);
-    this.userService.updateUser(this.userId, user).subscribe(() => {
-      this.router.navigate(['/users']);
-    });
-  } else {
+  isValidPhone(): boolean {
+    const phoneRegex = /^[0-9]{10}$/;
+    return phoneRegex.test(this.phone);
+  }
+
+  submit(): void {
+    if (this.email && !this.isValidEmail()) {
+      alert('Email invalide !');
+      return;
+    }
+    if (this.phone && !this.isValidPhone()) {
+      alert('Téléphone invalide — 10 chiffres requis !');
+      return;
+    }
+
     const user = new User(this.firstname, this.lastname, this.email, this.phone);
-    this.userService.createUser(user).subscribe(() => {
-      this.router.navigate(['/users']);
-    });
+    if (this.isEditMode && this.userId) {
+      user.id = this.userId;
+      this.userService.updateUser(this.userId, user).subscribe(() => {
+        this.router.navigate(['/users']);
+      });
+    } else {
+      this.userService.createUser(user).subscribe(() => {
+        this.router.navigate(['/users']);
+      });
+    }
   }
-}
+
   cancel(): void {
     this.router.navigate(['/users']);
   }
